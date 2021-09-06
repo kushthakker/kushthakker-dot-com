ac5fced2fa33bb3594c88949bc8c190961970d7f
kushthakker <kushthakker777@gmail.com>
2021-09-06 20:33:11 +0530

    added info on how to use swr

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,22 @@ import Head from "next/head";
 // import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import styled from "styled-components";
+// import useSWR from "swr";
+// import fetch from "../libs/fetch";
 
 export default function Home() {
+  // use swr
+
+  // const { data, error } = useSWR(url, fetch);
+
+  // if (error) {
+  //   return <div>{error.message}</div>;
+  // }
+
+  // if (!data) {
+  //   return <div>Loading...</div>;
+  // }
+
   return (
     <div className={styles.container}>
       <h1>Boilerplate</h1>
