eaecd1d3ff672005c26bfd500b22217789e39799
kushthakker <kushthakker777@gmail.com>
2021-09-11 14:57:45 +0530

    moved toggel to top left

--- components/DarkModeToggle/index.js
+++ components/DarkModeToggle/index.js
@@ -6,7 +6,7 @@ const DarkModeButton = () => {
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className="py-3 px-7 rounded-sm order-2 md:order-3 border ml-5 border-black dark:border-white hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black shadow-2xl dark:shadow-none transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 active:translate-y-3"
+      className="absolute top-5 left-5 py-3 px-7 rounded-sm order-2 md:order-3 border ml-5 border-black dark:border-white hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black shadow-2xl dark:shadow-none transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 active:translate-y-3"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       Toggle
