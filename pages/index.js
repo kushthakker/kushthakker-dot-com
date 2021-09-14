6c22a4f27c3d2f43fd148b1d222cdd9e45ffaf1e
kushthakker <kushthakker777@gmail.com>
2021-09-14 18:10:14 +0530

    added scroll to more

--- pages/index.js
+++ pages/index.js
@@ -17,57 +17,22 @@ import { useInView } from "react-intersection-observer";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 export default function Home() {
-  // const [ffLayer, setFfLayer] = useState(0);
   const [y, setY] = useState(0);
-  // const controls1 = useAnimation();
-  // const controls2 = useAnimation();
-  // const controls3 = useAnimation();
+
   const { scrollY, scrollYProgress } = useViewportScroll();
   const scaleAnim = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1.2, 0.8]);
-  // const [ref1, inView1] = useInView();
-  // const [ref2, inView2] = useInView();
-  // const [ref3, inView3] = useInView();
-  // const yPosAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 100, 200]);
-  // const zRotAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 3, 0]);
+
   const transition = {
     duration: 1,
     ease: [0.43, 0.13, 0.23, 0.96],
     delay: 0.3,
   };
 
-  // useEffect(() => {
-  //   if (inView1) {
-  //     controls1.start("visible");
-  //   }
-  //   if (inView2) {
-  //     controls2.start("visible");
-  //   }
-  //   if (inView3) {
-  //     controls3.start("visible");
-  //   }
-  // }, [controls1, inView1, controls2, inView2, controls3, inView3]);
-
-  // console.log(`scrollYProgress`, scrollYProgress);
-  // scrollYProgress.onChange((x) => {
-  //   setFfLayer(x > 0.4 ? -1 : 0);
-  // });
-
   scrollY.onChange((y) => {
     setY(y);
   });
 
   console.log(y);
-  // use swr
-
-  // const { data, error } = useSWR(url, fetch);
-
-  // if (error) {
-  //   return <div>{error.message}</div>;
-  // }
-
-  // if (!data) {
-  //   return <div>Loading...</div>;
-  // }
 
   return (
     <div className="grid grid-flow-row">
@@ -92,15 +57,18 @@ export default function Home() {
       </div>
       <div>
         <motion.div
-          // ref={ref1}
-          // animate={controls1}
-          // initial="hidden"
-          // transition={transition}
-          // variants={{
-          //   visible: { opacity: 1, scale: 1 },
-          //   hidden: { opacity: 0, scale: 0 },
-          // }}
-          // style={y >= 490 ? { display: "none" } : null}
+          initial={{ opacity: 0, y: -100 }}
+          animate={{
+            opacity: y >= 200 ? 0 : 1,
+            y: -130,
+            transition: { delay: 1.2, ...transition },
+          }}
+        >
+          <div className="flex justify-center items-center max-w-full">
+            Scroll to see more
+          </div>
+        </motion.div>
+        <motion.div
           initial={{ opacity: 0, y: 270 }}
           animate={{
             opacity: y >= 740 ? 0 : 1,
@@ -118,15 +86,6 @@ export default function Home() {
           </div>
         </motion.div>
         <motion.div
-          // ref={ref2}
-          // animate={controls2}
-          // initial="hidden"
-          // transition={transition}
-          // variants={{
-          //   visible: { opacity: 1, scale: 1 },
-          //   hidden: { opacity: 0, scale: 0 },
-          // }}
-          // style={y >= 900 ? { display: "none" } : null}
           initial={{ opacity: 0, y: 480 }}
           animate={{
             opacity: y >= 1470 ? 0 : 1,
@@ -139,15 +98,6 @@ export default function Home() {
           </h1>
         </motion.div>
         <motion.div
-          // ref={ref3}
-          // animate={controls3}
-          // initial="hidden"
-          // transition={transition}
-          // variants={{
-          //   visible: { opacity: 1, scale: 1 },
-          //   hidden: { opacity: 0, scale: 0 },
-          // }}
-          // style={y >= 1200 ? { display: "none" } : null}
           initial={{ opacity: 0, y: 680 }}
           animate={{
             opacity: y >= 2180 ? 0 : 1,
