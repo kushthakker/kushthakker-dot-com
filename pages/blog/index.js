2b8e664b69839a4b89f361b51b12afce8d094aed
kushthakker <kushthakker777@gmail.com>
2021-09-18 01:40:16 +0530

    centerd blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,14 @@
 const BlogPage = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="max-w-screen">
+      <div className="grid auto-rows-min gap-8 relative place-items-center top-40 mx-auto w-6/12">
+        <div>
+          <h1 className="text-new text-6xl">Blog</h1>
+        </div>
+        <div className="border-2 rounded w-full">Hello</div>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPage;
