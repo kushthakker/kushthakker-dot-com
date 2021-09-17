b8fabd3cdd9b14bdc575560c95750eb4c8c9a2c1
kushthakker <kushthakker777@gmail.com>
2021-09-17 18:32:27 +0530

    removed aria lable

--- components/Home.js
+++ components/Home.js
@@ -2,11 +2,7 @@ import Link from "next/link";
 
 const HomeButton = () => {
   return (
-    <span
-      aria-label="Home"
-      type="button"
-      className="py-2 px-4 sm:text-xl text-base"
-    >
+    <span className="py-2 px-4 sm:text-xl text-base">
       <Link href="/">Home</Link>
     </span>
   );
